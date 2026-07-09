58a36acf151c5fb3cf8dd156c1ea48179a219d4b
klbthedev <root@DESKTOP-GBQ5UHD.localdomain>
2026-07-09 17:04:10 +0300

    Added contract builder

--- contract_management/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import contract_variable
-from . import contract_clause
-from . import contract_article
-from . import contract_template
-from . import contract_variable_value
-from . import contract_contract
